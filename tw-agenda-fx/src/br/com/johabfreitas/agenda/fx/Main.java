43370543a9808ef2f4d1ca6159476fd47c0530c6
Johab Freitas <freitasjohab@gmail.com>
2022-09-12 11:08:13 -0300

    Update

---
+++ tw-agenda-fx/src/br/com/johabfreitas/agenda/fx/Main.java
@@ -0,0 +1,29 @@
+package br.com.johabfreitas.agenda.fx;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Minha Agenda");
+			primaryStage.setResizable(true);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
