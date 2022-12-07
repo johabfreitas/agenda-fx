29b99bdeb6e096d3223799b4dd0afc190a22ffee
Johab Freitas <freitasjohab@gmail.com>
2022-12-07 17:25:16 -0300

    Definindo a interface de exportação

---
+++ tw_agenda_servicos/src/br/com/johabfreitas/agenda/servicos/impl/BufferedWriterServicoContato.java
@@ -0,0 +1,16 @@
+package br.com.johabfreitas.agenda.servicos.impl;
+
+import java.util.List;
+
+import br.com.johabfreitas.agenda.entidades.Contato;
+import br.com.johabfreitas.agenda.servicos.interfaces.ServicoContato;
+
+public class BufferedWriterServicoContato implements ServicoContato{
+
+	@Override
+	public void exportar(List<Contato> contatos, String nomeArquivo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
