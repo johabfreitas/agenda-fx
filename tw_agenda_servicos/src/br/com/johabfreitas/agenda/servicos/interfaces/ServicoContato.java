00e51c4b48dec177504648ce5a4d0ae3ff6b8c17
Johab Freitas <freitasjohab@gmail.com>
2022-12-07 16:53:05 -0300

    Definindo a interface de exportação

---
+++ tw_agenda_servicos/src/br/com/johabfreitas/agenda/servicos/interfaces/ServicoContato.java
@@ -0,0 +1,10 @@
+package br.com.johabfreitas.agenda.servicos.interfaces;
+
+import java.util.List;
+
+import br.com.johabfreitas.agenda.entidades.Contato;
+
+public interface ServicoContato {
+	
+	public void exportar(List<Contato> contatos, String nomeArquivo);
+}
---
+++ tw_agenda_servicos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author johab
+ *
+ */
+module tw_agenda_servicos {
+}
