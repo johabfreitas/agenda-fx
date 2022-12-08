23a9808a5b43dab66f6f63c9865dc1c37eaee950
Johab Freitas <freitasjohab@gmail.com>
2022-12-08 12:24:11 -0300

    Utilizando o try-with-resources

--- tw_agenda_servicos/src/br/com/johabfreitas/agenda/servicos/impl/BufferedWriterServicoContato.java
+++ tw_agenda_servicos/src/br/com/johabfreitas/agenda/servicos/impl/BufferedWriterServicoContato.java
@@ -4,18 +4,31 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import br.com.johabfreitas.agenda.entidades.Contato;
 import br.com.johabfreitas.agenda.servicos.interfaces.ServicoContato;
 
-public class BufferedWriterServicoContato implements ServicoContato{
+public class BufferedWriterServicoContato implements ServicoContato {
 
 	@Override
-	public void exportar(List<Contato> contatos, String nomeArquivo) throws IOException{
+	public void exportar(List<Contato> contatos, String nomeArquivo) throws IOException {
 		// BufferedWrite (escrever) ---> FileWriter (apontador) ---> arquivo no HD
 		FileWriter fileWriter = new FileWriter(nomeArquivo);
-		BufferedWriter writer = new BufferedWriter(fileWriter); 
-		
+		try (BufferedWriter writer = new BufferedWriter(fileWriter)) {
+			List<String> dadosParaExportar = contatos.stream().map(contato -> String.format("%d;%s;%d;%s\n",
+					contato.getId(), contato.getNome(), contato.getIdade(), contato.getTelefone()))
+					.collect(Collectors.toList());
+			
+			/*// dadosParaExportar.forEach(linha -> writer.write(linha));
+			 * No Java expressões lambdas não podem lançar exceções
+			 */
+			
+			for (String linha : dadosParaExportar) {
+				writer.write(linha);
+			}
+		}
+
 	}
 
 }
