c0786269a3c32862f9e792fa5668904e02b479c1
Johab Freitas <freitasjohab@gmail.com>
2022-12-26 15:50:19 -0300

    JSTL, EL e JSPs

--- tw_agenda_servlets/src/main/java/br/com/johabfreitas/agenda/servlets/MensagemServlet.java
+++ tw_agenda_servlets/src/main/java/br/com/johabfreitas/agenda/servlets/MensagemServlet.java
@@ -1,8 +1,8 @@
 package br.com.johabfreitas.agenda.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,33 +14,14 @@ public class MensagemServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("	<head>");
-		out.println("		<title>Nome do usuário...</title>");
-		out.println("	</head>");
-		out.println("	<body>");
-		out.println("		<form action=\"/tw_agenda_servlets/mensagem\" method=\"post\">");
-		out.println("			<p>Digite seu nome:</p>");
-		out.println("			<p><input type=\"text\" name=\"nomeUsuario\"></p>");
-		out.println("			<p><button type=\"submit\">Enviar!</button></p>");
-		out.println("		</form>");
-		out.println("	</body>");
-		out.println("</html>");
+		RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/paginas/FormularioMensagem.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nomeUsuario = request.getParameter("nomeUsuario");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("	<head>");
-		out.println("		<title>Nome do usuário!</title>");
-		out.println("	</head>");
-		out.println("	<body>");
-		out.println("		<p>Olá, " + nomeUsuario + "</p>");
-		out.println("	</body>");
-		out.println("</html>");
+		RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/paginas/ResultadoMensagem.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
---
+++ tw_agenda_servlets/src/main/webapp/WEB-INF/paginas/ResultadoMensagem.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jstl/core" prefix="jstl" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mensagem</title>
+</head>
+<body>
+	<p>Olá, ${param.nomeUsuario}</p>
+</body>
+</html>
